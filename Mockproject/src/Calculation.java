4cd71837d2b9b81f94e68aa93507870d3d172145
RahulNomula <nomula.rahul001@gmail.com>
2018-11-26 22:41:47 -0400

    Changes by Rahul

---
+++ Mockproject/src/Calculation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Calculation {
+
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+     Scanner s=new Scanner(System.in);
+     int a,b;
+     a= s.nextInt();
+     b= s.nextInt();
+     add(a,b);
+//     sub(a,b);
+//     mul(a,b);
+//     div(a,b);
+//     min(a,b);
+//     max(a,b);
+     }
+	public static int add(int a, int b) {
+		int res= a+b;
+		return res;
+	}
+
+}
